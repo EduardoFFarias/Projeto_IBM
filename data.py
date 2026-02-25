4d760a996b7977f926c4f53063b59a38f1d599bb
Eduardo Oliveira <eduardofarias592@gmail.com>
2026-02-24 21:08:38 -0300

    feat: Insere analisa colunas, insere colunas

--- data.py
+++ data.py
@@ -2,7 +2,22 @@
 import pandas as pd
 import json
 
-df = pd.read_csv("P:\projeto\Projeto_IBM\data\data_ibm.csv")
+#Esta pegando o csv no meu local (Eduardo)
+df = pd.read_csv(r"C:\Users\eduar\Documents\Estudos\Projeto_IBM\data\data_ibm.csv")
 
 #%%
-df.head()
+#analise das colunas
+df.info()
+df.describe()
+df.isnull().sum() #verifica se tem informações nulas
+
+#%%
+#Cria coluna de faixa etaria para melhor analise no power bi, alem de melhor entedimento para usuario
+df["Faixa-etaria"] = pd.cut(
+    df["Age"],
+    bins=[18,25,35,45,60],
+    labels=["18-25", "26-35", "36-45", "46-60"]
+)
+
+df.head(10)
+
