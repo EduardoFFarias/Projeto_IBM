21f8d4677cc8a0287f742f85a98f9b824c994cc9
eduardo oliveira <eduardofarias592@gmail.com>
2026-03-12 16:24:14 -0300

    feat: insere coluna distancia do trabalho

--- noteboks/data.py
+++ noteboks/data.py
@@ -1,20 +1,36 @@
 #%%
 import pandas as pd
+import os
 import json
 
-df = pd.read_csv(r"data\data_ibm.csv")
 
-#%% ANALISE DAS COLUNAS
+# Pega o local onde este script está salvo
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Define os caminhos de entrada e saída voltando uma pasta para 'data'
+input_path = os.path.join(base_path, "..", "data", "data_ibm.csv")
+output_path = os.path.join(base_path, "..", "data", "data_ibm_tratado.csv")
+
+
+try:
+    df = pd.read_csv(input_path)
+    print("Arquivo carregado com sucesso!")
+except FileNotFoundError:
+    print(f"ERRO: Arquivo não encontrado em {input_path}")
+    exit()
+
+#ANALISE DAS COLUNAS
 df.isnull().sum()
 df.info()
 df.head()
 
-#%% CRIAÇÃO DE COLUNAS
+# CRIAÇÃO DE COLUNAS
 
 df["Faixa-etaria"] = pd.cut(
     df["Age"],
     bins=[18,25,35,45,60],
-    labels=["18-25", "26-35", "36-45", "46-60"]
+    labels=["18-25", "26-35", "36-45", "46-60"],
+    include_lowest=True
 )
 
 df["nivel_salario"] = pd.qcut(
@@ -30,6 +46,12 @@ df["tempo_empresa"] = pd.cut(
     include_lowest=True
 )
 
+df["distancia_casa"] = pd.cut(
+    df["DistanceFromHome"],
+    bins=[0,5,15, float('inf')],
+    labels=["Perto (0-5km)", "Médio (6-15km)", "Longe (+15km)"]
+)
+
 df["Attrition_num"] = (
     df["Attrition"]
     .astype(str)
@@ -37,12 +59,13 @@ df["Attrition_num"] = (
     .map({"Yes":1, "No":0})
 )
 
-#%% ANALISE EXPLORATORIA
+# ANALISE EXPLORATORIA
 
 print(f"{df['Attrition_num'].mean() * 100:.2f}%")
 print(df.groupby("Faixa-etaria")["Attrition_num"].mean())
 print(df.groupby("tempo_empresa")["Attrition_num"].mean())
 
-#%% EXPORTAÇÃO PARA POWER BI
+# EXPORTAÇÃO PARA POWER BI
 
-df.to_csv("data/data_ibm_tratado.csv", index=False)
+df.to_csv(output_path, index=False)
+print(f"Arquivo tratado exportado com sucesso para: {output_path}")
